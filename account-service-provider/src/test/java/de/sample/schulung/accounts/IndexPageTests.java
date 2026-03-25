718cc2790e36469ef5705635b921995247c06dac
Ralf Ueberfuhr <ralf.ueberfuhr@yahoo.com>
2026-03-25 14:05:48 +0100

    Upgrade Spring Boot to 4.0.4 and update test annotations and dependencies accordingly.

--- account-service-provider/src/test/java/de/sample/schulung/accounts/IndexPageTests.java
+++ account-service-provider/src/test/java/de/sample/schulung/accounts/IndexPageTests.java
@@ -2,9 +2,9 @@ package de.sample.schulung.accounts;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
--- account-service-provider/src/test/java/de/sample/schulung/accounts/domain/CustomersServiceTest.java
+++ account-service-provider/src/test/java/de/sample/schulung/accounts/domain/CustomersServiceTest.java
@@ -2,7 +2,7 @@ package de.sample.schulung.accounts.domain;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
